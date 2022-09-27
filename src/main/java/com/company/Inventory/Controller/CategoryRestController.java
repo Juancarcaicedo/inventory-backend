5c84343b5b7e915381ae83bb5ce9a75f7a1de122
juancarcaicedo <juancarcaicedo@unicauca.edu.co>
2022-09-26 19:52:55 -0500

    Actualizacion del codigo

--- src/main/java/com/company/Inventory/Controller/CategoryRestController.java
+++ src/main/java/com/company/Inventory/Controller/CategoryRestController.java
@@ -2,6 +2,7 @@ package com.company.Inventory.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,7 @@ import com.company.Inventory.Services.iCategoryService;
 import com.company.Inventory.model.Category;
 import com.company.Inventory.response.CategoryResponseRest;
 
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 @RequestMapping("/api/v1")
 
